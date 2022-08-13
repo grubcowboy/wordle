571cced119241ad3019f25f7ce22fa8785415a3d
J Woodson <jasmine.a.woodson@gmail.com>
2022-08-12 18:27:59 -0700

    added function to choose a word from words vector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::io;
+use rand::{thread_rng, Rng};
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
-//const WORD_LENGTH = 5;
+const WORD_LENGTH: u8 = 5;
 
 fn main() {
+    let words = get_words(WORD_LENGTH);
+
+    println!("words.len(): {}", words.len());
+
+    let mut rng = thread_rng();
+    let answer: usize = rng.gen_range(0, words.len());
+    println!("{}", answer);
+
+    let solution = &words[answer];
+    println!("{}", solution);
+
     let mut attempt_count = 0;
     let max_guesses = 6;
     let mut correct = false;
@@ -12,7 +25,7 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap(); // TODO: Handle err
-        correct = check_answer(input);
+        correct = check_answer(solution, input);
 
         attempt_count += 1;
     }
@@ -24,12 +37,31 @@ fn main() {
     }
 
     // TODO:
+    // * Don't hard code the word
     // * Add basic UI
 }
 
-fn check_answer(s: String) -> bool {
-    let answer = "heart".to_string();
+fn get_words(length: u8) -> Vec<String> {
+    let file = File::open("/usr/share/dict/words").unwrap();
+    let reader = BufReader::new(file);
+    let mut words = vec![];
+
+    for line in reader.lines() {
+        //println!("{}", line.unwrap());
+        let word = line.unwrap();
+        if word.trim().len() as u8 == length {
+            words.push(word);
+        }
+    }
+
+    // TODO: Finish
+    return words;
+}
+
+fn check_answer(sol: &String, s: String) -> bool {
+    let answer = sol.to_string();
+    println!("{}", answer);
     let s = s.trim();
 
-    return answer == s;
+    answer == s
 }
